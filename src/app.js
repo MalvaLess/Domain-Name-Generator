bc63290b1dc36b9902463b0bee006dd297e3a638
MalvaLess <spider3899@gmail.com>
2026-02-27 20:54:25 -0300

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-import "bootstrap";
-import "./style.css";
+// Base array
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extensions = ['.com', '.net', '.us', '.io'];
 
+// <ul> Where results are shown
+const domainList = document.getElementById("domain-list");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// Domain generator
+for (let p of pronoun) {
+  for (let a of adj) {
+    for (let n of noun) {
+      for (let ext of extensions) {
+        let domain = p + a + n + ext;
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+        // If noun ends same as extension
+        if (n.endsWith(ext.replace('.', ''))) {
+          domain = p + a + n.replace(ext.replace('.', ''), '') + ext;
+        }
+
+        console.log(domain);
+
+        // Show on page
+        let li = document.createElement("li");
+        li.textContent = domain;
+        domainList.appendChild(li);
+      }
+    }
+  }
+}
